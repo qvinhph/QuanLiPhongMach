63371b204f4df39e8557cb33b1dbd6b39e7c5650
Quang Nguyễn Đăng <16521560@ms.uit.edu.vn>
2018-06-28 15:44:41 +0700

    Quang: add BenhNhanBUS

--- QLPMGUI/frmLapDanhSachKham.vb
+++ QLPMGUI/frmLapDanhSachKham.vb
@@ -4,7 +4,7 @@ Imports Utility
 
 Public Class frmLapDanhSachKham
 
-    Private dgBus As PhongmachBUS
+    Private bnBus As BenhNhanBUS
 
     Private Sub btnNhap_Click(sender As Object, e As EventArgs) Handles btnNhap.Click
 
@@ -16,15 +16,11 @@ Public Class frmLapDanhSachKham
         Benhnhan.HoTen = txtHoTen.Text
         Benhnhan.DiaChi = txtDiaChi.Text
         Benhnhan.NgaySinh = dtpNgaySinh.Value
-
-
-
-
-
-
+        Benhnhan.NgayKham = dtpNgayKham.Text
+        Benhnhan.Gioitinh = cbGioitinh.Text
 
         '2. Business .....
-        If (dgBus.isValidName(Benhnhan) = False) Then
+        If (bnBus.isValidName(Benhnhan) = False) Then
             MessageBox.Show("Họ tên độc giả không đúng")
             txtHoTen.Focus()
             Return
@@ -32,19 +28,38 @@ Public Class frmLapDanhSachKham
 
 
 
-        ''3. Insert to DB
+        '3. Insert to Listview
+        Dim lvItem As ListViewItem
+        lvItem = lvBenhNhan.Items.Add(txtHoTen.Text)
+        lvItem.SubItems.Add(txtMaSo.Text)
+        lvItem.SubItems.Add(txtDiaChi.Text)
+        lvItem.SubItems.Add(dtpNgaySinh.Value)
+        lvItem.SubItems.Add(cbGioitinh.Text)
+        lvItem.SubItems.Add(dtpNgayKham.Value)
+
+        'Auto generate next patient id
+
+        txtMaSo.Text = txtMaSo.Text + 1
+
+
+
+
+
+
+
+
         'Dim result As Result
-        'result = dgBus.insert(Benhnhan)
+        'result = bnBus.insert(Benhnhan)
         'If (result.FlagResult = True) Then
         '    MessageBox.Show("Thêm độc giả thành công.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information)
-        '    Dim nextMsdg = "1"
-        '    result = dgBus.buildMSDG(nextMsdg)
+        '    Dim nextMsbn = "1"
+        '    result = bnBus.buildMSBN(nextMsbn)
         '    If (result.FlagResult = False) Then
         '        MessageBox.Show("Lấy danh tự động mã độc giả không thành công.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
         '        Me.Close()
         '        Return
         '    End If
-        '    txtMaSo.Text = nextMsdg
+        '    txtMaSo.Text = nextMsbn
         '    txtDiaChi.Text = String.Empty
         '    txtHoTen.Text = String.Empty
 
@@ -57,26 +72,27 @@ Public Class frmLapDanhSachKham
 
     Private Sub frmLapDanhSachKhamGUI_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+        dtpNgayKham.Value = Format(Date.Now(), "ddMMMyyyy")
 
         'Auto generate patient id
-        dgBus = New PhongmachBUS()
+        bnBus = New BenhNhanBUS()
         Dim result
-        Dim nextMsdg = "1"
-        result = dgBus.buildMSDG(nextMsdg)
+        Dim nextMsbn = "1"
+        result = bnBus.buildMSBN(nextMsbn)
         If (result.FlagResult = False) Then
             MessageBox.Show("Lấy danh sách tự động mã bệnh nhân không thành công.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
             System.Console.WriteLine(result.SystemMessage)
             Me.Close()
             Return
         End If
-        txtMaSo.Text = nextMsdg
+        txtMaSo.Text = nextMsbn
 
         '
-        lvBenhNhan.Columns.Add("", 0)
-        lvBenhNhan.Columns.Add("Họ Tên Bệnh Nhân", 150, HorizontalAlignment.Center)
-        lvBenhNhan.Columns.Add("Mã số", 80, HorizontalAlignment.Center)
-        lvBenhNhan.Columns.Add("Địa Chỉ", 140, HorizontalAlignment.Center)
-        lvBenhNhan.Columns.Add("Giới Tính", 80, HorizontalAlignment.Center)
+        lvBenhNhan.Columns.Add("Họ Tên Bệnh Nhân", 60, HorizontalAlignment.Center)
+        lvBenhNhan.Columns.Add("Mã số", 50, HorizontalAlignment.Center)
+        lvBenhNhan.Columns.Add("Địa Chỉ", 130, HorizontalAlignment.Center)
+        lvBenhNhan.Columns.Add("Ngày Sinh", 100, HorizontalAlignment.Center)
+        lvBenhNhan.Columns.Add("Giới Tính", 70, HorizontalAlignment.Center)
         lvBenhNhan.Columns.Add("Ngày Khám", 100, HorizontalAlignment.Center)
 
 
@@ -94,14 +110,14 @@ Public Class frmLapDanhSachKham
 
 
         '2. Business .....
-        If (dgBus.isValidName(Benhnhan) = False) Then
+        If (bnBus.isValidName(Benhnhan) = False) Then
             MessageBox.Show("Họ tên độc giả không đúng")
             txtHoTen.Focus()
             Return
         End If
         '3. Insert to DB
         Dim result As Result
-        result = dgBus.insert(Benhnhan)
+        result = bnBus.insert(Benhnhan)
         If (result.FlagResult = True) Then
             MessageBox.Show("Thêm độc giả thành công.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information)
             Me.Close()
@@ -163,7 +179,7 @@ Public Class frmLapDanhSachKham
 
     End Sub
 
-    Private Sub txtNgaylapthe_Click(sender As Object, e As EventArgs) Handles txtNgaylapthe.Click
+    Private Sub txtNgaylapthe_Click(sender As Object, e As EventArgs)
 
     End Sub
 
@@ -204,5 +220,9 @@ Public Class frmLapDanhSachKham
 
     Private Sub lvBenhNhan_SelectedIndexChanged(sender As Object, e As EventArgs) Handles lvBenhNhan.SelectedIndexChanged
 
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Application.Exit()
     End Sub
 End Class
--- QLPMGUI/frmTest.vb
+++ QLPMGUI/frmTest.vb
@@ -12,4 +12,9 @@ Public Class frmTest
         Dim result = donViDAL.BuildID(nextID)
         lbMaDonvi.Text = nextID
     End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim lapdskham As frmLapDanhSachKham = New frmLapDanhSachKham()
+        lapdskham.Show()
+    End Sub
 End Class
